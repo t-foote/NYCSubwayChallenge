57a67d8501ba0016c166e45d3c6ad57052064323
Trevor <trevor.foote@mail.utoronto.ca>
2025-05-05 11:07:16 -0400

    Repo setup and basic UI mockup

---
+++ frontend/app/(tabs)/map.tsx
@@ -0,0 +1,49 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
+
+
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+
+const LATITUDE = 40.7249;
+const LONGITUDE = -73.9240;
+const LATITUDE_DELTA = 0.461;
+const LONGITUDE_DELTA = 0.211;
+
+export default function MapScreen() {
+  const [locationGranted, setLocationGranted] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      setLocationGranted(status === 'granted');
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+        showsUserLocation={locationGranted}
+      >
+        {/* <Marker
+          coordinate={{ latitude: LATITUDE, longitude: LONGITUDE }}
+          title="Marker Title"
+          description="Marker description"
+        /> */}
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  map: { flex: 1 },
+});
